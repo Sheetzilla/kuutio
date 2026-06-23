5ed508dd56488171e8950f6b302e328e31b781ad
sheet <sheetzillacoms@gmail.com>
2026-06-23 23:11:17 +0300

    email

--- api/kofi.js
+++ api/kofi.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      sender: { name: 'FishTimer', email: 'sleetsheet.st@gmail.com' },
+      sender: { name: 'FishTimer', email: 'sheet@sleetsheet.com' },
       to: [{ email: data.email }],
       subject: 'Your FishTimer license key',
       htmlContent: `
